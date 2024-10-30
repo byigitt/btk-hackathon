5adf2491a8e2848e182d7065b47a72f9438bf4c9
byigitt <c2228009@student.cankaya.edu.tr>
2024-10-30 19:52:48 +0300

    feat/constants: add constants for google search

---
+++ backend/src/utils/constants.ts
@@ -0,0 +1,19 @@
+export const URLS = {
+    GOOGLE: 'https://google.com/',
+    W_GOOGLE: 'https://www.google.com/',
+    FAVICONKIT: 'https://api.faviconkit.com'
+};
+
+export const SELECTORS = {
+    // Organic Search Results 
+    TITLE: 'div.kb0PBd.A9Y9g.jGGQ5e > div > div > span > a > h3',
+    DESCRIPTION: 'div.VwiC3b.yXK7lf.lVm3ye.r025kc.hJNv6b.Hdw6tb > span',
+    URL: 'div.kb0PBd.A9Y9g.jGGQ5e > div > div > span > a',
+
+    // Videos
+    VIDEO_TITLE: 'div > div > div.xe8e1b > div > div > span > a > h3',
+    VIDEO_DESCRIPTION: 'div.ITZIwc',
+    VIDEO_URL: 'div > div > div.xe8e1b > div > div > span > a',
+    VIDEO_THUMBNAIL: 'div > div > div > div.iHxmLe > a > div > div > div.uhHOwf.BYbUcd > img',
+    VIDEO_DURATION: 'div.c8rnLc.flgn0c.k1U36b.zCaigb > span'
+}; 
